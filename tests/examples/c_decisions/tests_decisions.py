49a13980e72e5bde43d8eeeb975e0e69b4bed73a
Miriam Cruz <miriam.cruz@g.austincc.edu>
2025-02-13 02:48:50 +0000

    test and, or, and not truth tables

--- tests/examples/c_decisions/tests_decisions.py
+++ tests/examples/c_decisions/tests_decisions.py
@@ -7,3 +7,21 @@ class Test_Config(unittest.TestCase):
     def test_configuration(self):
         self.assertEqual(True, test_config())
 
+    def test_and_truth_table(self):
+
+        self.assertEqual(False and False, False)
+        self.assertEqual(True and False, False)
+        self.assertEqual(False and True, False)
+        self.assertEqual(True and True, True)
+
+    def test_or_truth_table(self):
+
+        self.assertEqual(False or False, False)
+        self.assertEqual(True or False, True)
+        self.assertEqual(False or True, True)
+        self.assertEqual(True or True, True)
+
+    def test_not_truth_table(self):
+
+        self.assertEqual(not False, True)
+        self.assertEqual(not True, False) 
